00cf9ad787b59cf5a9bbb99684178a10ace0c9b6
John Niesman <john-niesman@uiowa.edu>
2023-02-08 15:28:29 -0600

    added user model

--- src/main/java/com/jpniesman/springdemo/Models/User.java
+++ src/main/java/com/jpniesman/springdemo/Models/User.java
@@ -1,2 +1,10 @@
-package com.jpniesman.springdemo.Models;public class User {
+package com.jpniesman.springdemo.Models;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String DoB;
 }
