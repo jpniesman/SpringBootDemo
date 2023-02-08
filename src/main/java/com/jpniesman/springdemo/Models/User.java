b0ed6ef5e6a294dba9d6a9658f98ef3276030cd2
John Niesman <john-niesman@uiowa.edu>
2023-02-08 15:39:45 -0600

    added getters and setters for user model

--- src/main/java/com/jpniesman/springdemo/Models/User.java
+++ src/main/java/com/jpniesman/springdemo/Models/User.java
@@ -7,4 +7,44 @@ public class User {
     private String lastName;
     private int age;
     private String DoB;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
 }
