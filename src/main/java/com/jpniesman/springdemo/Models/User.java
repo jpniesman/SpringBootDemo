3ee72e3d00735f50b034dbb9f9e4f343057fcd96
John Niesman <john-niesman@uiowa.edu>
2023-02-09 13:10:02 -0600

    switched getters and setters to lombok

--- src/main/java/com/jpniesman/springdemo/Models/User.java
+++ src/main/java/com/jpniesman/springdemo/Models/User.java
@@ -1,8 +1,12 @@
 package com.jpniesman.springdemo.Models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -20,43 +24,4 @@ public class User {
     @Column
     private String DoB;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getDoB() {
-        return DoB;
-    }
-
-    public void setDoB(String doB) {
-        DoB = doB;
-    }
 }
